d66fcb43dc20d03534183e068e5f8ee27fa0570b
Jose Clemente <jclemente.tlm@gmail.com>
2025-08-12 14:37:24 -0500

    wip: actualiza diagrama de implementacion generadas

--- export-diagrams.sh
+++ export-diagrams.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
 node ./export-diagrams.js http://localhost:8090/workspace/diagrams png ./diagrams/servicios-corporativos
+
+# Eliminar imágenes con sufijo -key.png
+target_dir=./diagrams/servicios-corporativos
+find "$target_dir" -type f -name '*-key.png' -delete
